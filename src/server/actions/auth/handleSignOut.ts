ea19e0e14b94a95bb48760d827ac2b9cc48618c0
mamun2519 <juborajislam46@gmail.com>
2026-06-24 11:30:18 +0600

    fix: await headers() in handleSignOut

--- src/server/actions/auth/handleSignOut.ts
+++ src/server/actions/auth/handleSignOut.ts
@@ -5,7 +5,8 @@ import appConfig from '@/configs/app.config'
 import { headers } from 'next/headers'
 
 const handleSignOut = async () => {
-    const host = headers().get('host')
+    const headersList = await headers()
+    const host = headersList.get('host')
     const protocol = host?.includes('localhost') ? 'http' : 'https'
     const redirectTo = `${protocol}://${host}${appConfig.unAuthenticatedEntryPath}`
     await signOut({ redirectTo })
