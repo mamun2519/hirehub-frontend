ef88d68c572fb7ccdbe612a8e94e8dde49cb5201
mamun2519 <juborajislam46@gmail.com>
2026-06-24 11:23:30 +0600

    fix: dynamic signout redirect and correct signin routing

--- src/server/actions/auth/handleSignOut.ts
+++ src/server/actions/auth/handleSignOut.ts
@@ -2,9 +2,13 @@
 
 import { signOut } from '@/auth'
 import appConfig from '@/configs/app.config'
+import { headers } from 'next/headers'
 
 const handleSignOut = async () => {
-    await signOut({ redirectTo: appConfig.unAuthenticatedEntryPath })
+    const host = headers().get('host')
+    const protocol = host?.includes('localhost') ? 'http' : 'https'
+    const redirectTo = `${protocol}://${host}${appConfig.unAuthenticatedEntryPath}`
+    await signOut({ redirectTo })
 }
 
 export default handleSignOut
